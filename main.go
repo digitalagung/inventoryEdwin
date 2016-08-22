127e3ab24caadc2198a114ed9036a30d4af70aa3
edwinlba <edwinuad@gmail.com>
2016-08-22 21:53:49 +0700

    setting up project, add glide and db migration

---
+++ main.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"github.com/labstack/echo"
+	"github.com/labstack/echo/engine/fasthttp"
+	"github.com/labstack/echo/middleware"
+
+	"github.com/edwinlab/inventory/user"
+)
+
+func main() {
+	e := echo.New()
+
+	// Middleware
+	e.Use(middleware.Logger())
+	e.Use(middleware.Recover())
+
+	// Routes
+	e.POST("/users", user.CreateUser)
+	e.GET("/users/:id", user.GetUser)
+	e.PUT("/users/:id", user.UpdateUser)
+	e.DELETE("/users/:id", user.DeleteUser)
+
+	// Start server
+	e.Run(fasthttp.New(":1323"))
+}
